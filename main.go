6b70deda65d1de20dc81c572433bd57c23644f5f
Isaac Domini <7013557+isaacdomini@users.noreply.github.com>
2022-02-12 09:56:14 -0500

    Google DNS update works

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type DNSCred map[string]string
+
+func main() {
+	http.HandleFunc("/udpate", updateIP)
+	http.HandleFunc("/_ping", ping)
+	fmt.Println("Server started")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func updateIP(w http.ResponseWriter, r *http.Request) {
+	// fmt.Println(r)
+	ip := r.FormValue("ip")
+	errCount := 0
+	for _, cred := range dnsSecrets() {
+		params := url.Values{}
+		params.Add("hostname", cred["hostname"])
+		params.Add("myip", ip)
+		googUrl := fmt.Sprintf("https://%s:%s@domains.google.com/nic/update?%s", cred["username"], cred["password"], params.Encode())
+		_, err := http.Get(googUrl)
+		if err != nil {
+			log.Fatalln(err)
+			errCount += 1
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if errCount > 0 {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write([]byte(fmt.Sprintln(`{"status":"error updating `, errCount, ` hostnames"}`)))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status":"ok"}`))
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"status":"ok"}`))
+}
+
+func dnsSecrets() []DNSCred {
+	f, err := os.Open("./data/dns_secrets.yaml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	dec := yaml.NewDecoder(f)
+
+	var dnsCreds []DNSCred
+
+	err = dec.Decode(&dnsCreds)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dnsCreds
+}
